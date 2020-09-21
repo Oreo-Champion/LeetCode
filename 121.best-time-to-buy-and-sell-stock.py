fc1b5665260cc090cecad0bc117cee5ce37fbc9c
You Xie <xyshell@bu.edu>
2020-09-21 18:02:37 -0400

    update 121.best-time-to-buy-and-sell-stock

--- 121.best-time-to-buy-and-sell-stock.py
+++ 121.best-time-to-buy-and-sell-stock.py
@@ -6,19 +6,31 @@ from typing import List
 #
 # @lc code=start
 class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        max_sum, temp_sum = nums[0], 0
-        for num in nums:
-            temp_sum = max(num, num+temp_sum)
-            max_sum = max(max_sum, temp_sum)
-        return max_sum
+    # def maxSubArray(self, nums: List[int]) -> int:
+    #     max_sum, temp_sum = nums[0], 0
+    #     for num in nums:
+    #         temp_sum = max(num, num+temp_sum)
+    #         max_sum = max(max_sum, temp_sum)
+    #     return max_sum
+    
+    # def maxProfit(self, prices: List[int]) -> int:
+    #     if len(prices) < 2: return 0
+    #     ret = []
+    #     for i in range(len(prices) - 1):
+    #         ret.append(prices[i+1] - prices[i])
+    #     return max(0, self.maxSubArray(ret))
     
     def maxProfit(self, prices: List[int]) -> int:
-        if len(prices) < 2: return 0
-        ret = []
-        for i in range(len(prices) - 1):
-            ret.append(prices[i+1] - prices[i])
-        return max(0, self.maxSubArray(ret))
+        n  = len(prices)
+        if n < 2: return 0
+        last_low = prices[0]
+        max_profit = 0
+        for i in range(1, n):
+            profit = prices[i] - last_low
+            if profit < 0:
+                last_low = prices[i]
+            max_profit = max(max_profit, profit)
+        return max_profit
 
 # @lc code=end
 
