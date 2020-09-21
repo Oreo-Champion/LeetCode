ecaec40d1bb9e23582fdd6173e0f45c5c164d2b1
You Xie <xyshell@bu.edu>
2020-09-21 14:20:31 -0400

    update 350.intersection-of-two-arrays-ii other's sol

--- 350.intersection-of-two-arrays-ii.py
+++ 350.intersection-of-two-arrays-ii.py
@@ -8,13 +8,16 @@ from collections import Counter
 
 # @lc code=start
 class Solution:
+    # def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+    #     cntr1, cntr2 = Counter(nums1), Counter(nums2)
+    #     ans = []
+    #     for elem in cntr1:
+    #         count = min(cntr1[elem], cntr2[elem])
+    #         ans.extend([elem]*count)
+    #     return ans
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        cntr1, cntr2 = Counter(nums1), Counter(nums2)
-        ans = []
-        for elem in cntr1:
-            count = min(cntr1[elem], cntr2[elem])
-            ans.extend([elem]*count)
-        return ans
+        return (Counter(nums1) & Counter(nums2)).elements()
+    
 # @lc code=end
 
 Solution().intersect(nums1 = [4,9,5], nums2 = [9,4,9,8,4])
