1aefb738fb608b0882a79ceb6fef8e5e2f9b9820
Asgardawn <43394809+AsgardawnGH@users.noreply.github.com>
2020-09-22 05:59:32 +0800

    Add files via upload

--- LC121.py
+++ LC121.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 
 
-def LC121_MaxProfit2(prices: List[int]) -> int:
+def LC121_MaxProfit3(prices: List[int]) -> int:
     n = len(prices)
     if n <= 1:
         return 0
-    p = [0] * n
-    cl = [0] * n
-    cl[0] = prices[0]
+    p = 0
+    cl = prices[0]
     for i in range(1,n):
-        cl[i] = min(cl[i-1], prices[i])
-        p[i] = prices[i] - cl[i]
-    return max(p)
+        cl = min(cl, prices[i])
+        p = max(p, prices[i] - cl)
+    return p
