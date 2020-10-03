1956a574222ca4042caeb90039f12dd52dedd5b2
You Xie <xyshell@bu.edu>
2020-10-03 12:30:58 -0400

    69.sqrt-x.py

---
+++ 69.sqrt-x.py
@@ -0,0 +1,13 @@
+import math
+#
+# @lc app=leetcode id=69 lang=python3
+#
+# [69] Sqrt(x)
+#
+
+# @lc code=start
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        return int(math.sqrt(x))
+# @lc code=end
+
