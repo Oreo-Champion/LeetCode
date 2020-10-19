c0903358515e14a05fbd55fdf43a64724c231ca8
You Xie <xyshell@bu.edu>
2020-10-19 12:32:16 -0400

    191.number-of-1-bits.py

---
+++ 191.number-of-1-bits.py
@@ -0,0 +1,13 @@
+import collections
+#
+# @lc app=leetcode id=191 lang=python3
+#
+# [191] Number of 1 Bits
+#
+
+# @lc code=start
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        return collections.Counter(str(bin(n))[2:])['1']
+# @lc code=end
+Solution().hammingWeight()
